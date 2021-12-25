d951ab25d90011bfe5c588b40d5e75c22564e277
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2021-12-25 17:24:37 +0100

    Ignore unused variable in D13

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -19,8 +19,7 @@ import NoUnused.Modules
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
-import Review.Rule exposing (Rule)
-
+import Review.Rule exposing (Rule, ignoreErrorsForFiles)
 
 config : List Rule
 config =
@@ -31,5 +30,5 @@ config =
     , NoUnused.Modules.rule
     , NoUnused.Parameters.rule
     , NoUnused.Patterns.rule
-    , NoUnused.Variables.rule
+    , NoUnused.Variables.rule |> ignoreErrorsForFiles [ "src/Day13.elm" ]
     ]
